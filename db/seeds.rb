52e788694c183f32a8948dacd2a4b1733320709d
pbomba <paul@paulbomba.com>
2018-06-18 17:01:24 -0400

    added some methods in run.rb for later use

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,31 @@
 # Weapon.create(name: "Axe", min_damage: 2, max_damage: 7, armor_value: 0)
 # Weapon.create(name: "Spear", min_damage: 2, max_damage: 5, armor_value: 0)
 
+# def roll_attribute
+#   tot = rand(6) + 1
+#   tot += rand(6) + 1
+#   tot += rand(6) +1
+#   puts tot
+# end
+
+# def strength_bonus
+# 	if :strength >= 18
+# 		bonus = 4
+# 	elsif :strength >= 16
+# 		bonus = 3
+# 	elsif :strength >= 14
+# 		bonus = 2
+# 	elsif :strength >= 12
+# 		bonus = 1		
+# 	elsif :strength >= 10
+# 		bonus = 0
+# 	elsif :strength >= 8
+# 		bonus = -1
+# 	elsif :strength >= 6
+# 		bonus = -2
+# 	elsif :strength >= 4
+# 		bonus = -3
+# 	else
+# 		bonus = -4
+# 	end
+# end
--- lib/models/character.rb
+++ lib/models/character.rb
@@ -1,2 +1,5 @@
 class Character < ActiveRecord::Base
+	belongs_to :player
+	has_many :weapons
+	has_many :battles
 end
